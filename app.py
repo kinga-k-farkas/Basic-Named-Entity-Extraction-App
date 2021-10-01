6d0dd492497fca1664df82e0fea7919c74b0c330
kinga-k-farkas <kinga.k.farkas@gmail.com>
2021-09-30 21:45:53 -0700

    added title to app

--- app.py
+++ app.py
@@ -21,11 +21,8 @@ address_ner = ss('address_ner')
 drug_ner = ss('drug_ner')
 ner = ss('ner')
 
-# text = [''' I live at 3406 SE King Road, New York.  I used to live at str Magurei 3/17.  But that is not in Oregon.   We also lived on SE Taylor street.
-# Now someone is at 2566 SE Flavel, Portland, OR97822.    The Church is located at 555 East Main St.  The 555 East Main Avenue is the home to Marky Mark, who was born in 18.
-# The address of the State Street Corp. is 1041 Shoppes Blvd. and the.   The address of Revlon Inc is 1109 S Park St. and there are 70
-# The address of Cornelius is 305 N Veterans Pkwy. ''']
 display_dict = {"ADDRESS": "Street Addresses", "PHONE_NUMBER": "Phone Numbers", "DRUG": "Drugs"}
+
 
 def get_ents( sentence, nlp):
     doc = nlp(sentence)
@@ -37,12 +34,9 @@ def get_ents( sentence, nlp):
             ent_dict[ent.label_].append(ent)
     return ent_dict
 
-
-# for doc in address_ner.pipe(text, disable=["tagger", "parser"]):
-#     print([(ent.text, ent.label_) for ent in doc.ents], "\n")
-
-
-query = st.text_area('Enter your query:')
+st.subheader("Gathering Basic Information with NER")
+st.caption("Extracting Street Addresses, Phone Numbers, Drug Names and Generic Named Entities from Text")
+query = st.text_area('Enter your text:')
 if len(query) == 0:
     st.write('Entities:')
 else:
